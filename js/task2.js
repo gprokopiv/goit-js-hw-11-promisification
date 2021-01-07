b363671580b2420d8e09efbc6434a88d9f6befef
galyna <65094424+gprokopiv@users.noreply.github.com>
2021-01-07 18:08:52 +0200

    task2

--- js/task2.js
+++ js/task2.js
@@ -9,27 +9,20 @@ const users = [
   { name: 'Ajax', active: true },
   { name: 'Lux', active: false },
 ];
+
 const toggleUserState = (allUsers, userName) => {
   return new Promise ((resolve, reject) => {
-    const updatedUsers = allUsers.map(user);
-})
-  if (user.name === userName) {
-      active: !user.active  } 
-  const error = user, ;
-  reject(error);
-  // callback(updatedUsers);
-};
-
+    const updatedUsers = allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user,
+    );
+    setTimeout(() => {
+      resolve(updatedUsers);
+    }, 2000);
+  });
+}
 const logger = updatedUsers => console.table(updatedUsers);
 
-/*
- * Сейчас работает так
- */
-toggleUserState(users, 'Mango', logger);
-toggleUserState(users, 'Lux', logger);
 
-/*
- * Должно работать так
- */
+
 toggleUserState(users, 'Mango').then(logger);
 toggleUserState(users, 'Lux').then(logger);
